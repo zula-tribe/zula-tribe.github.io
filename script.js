1377616882228c1e90d86405c4e4d09855c84428
Ansu Darboe <ansu@zula-tribe.com>
2023-09-20 02:12:12 +0200

    added a header

--- script.js
+++ script.js
@@ -1,9 +1,43 @@
-function changeHeaderColor() {
-    const header = document.querySelector('header');
-    const currentColor = header.style.backgroundColor;
-    const newColor = currentColor === 'rgb(0, 51, 102)' ? '#ff9900' : '#003366';
-    header.style.backgroundColor = newColor;
-}
+document.addEventListener("DOMContentLoaded", function () {
+    const menuButton = document.getElementById("menuButton");
+    const menu = document.getElementById("menu");
 
-const header = document.querySelector('header');
-header.addEventListener('click', changeHeaderColor);
+    menuButton.addEventListener("click", function () {
+        menu.style.display = menu.style.display === "block" ? "none" : "block";
+    });
+
+    const sections = [
+        { label: "About", id: "about" },
+        { label: "Our Mission", id: "mission" },
+        { label: "Our Goals", id: "goals" },
+        { label: "Our Vision", id: "vision" },
+        { label: "Our Values", id: "our-values" },
+    ];
+
+    function generateMenuItems() {
+        const menuList = document.createElement("ul");
+        menuList.classList.add("list-unstyled");
+        menuList.classList.add("menu-link");
+
+        sections.forEach((section) => {
+            const menuItem = document.createElement("li");
+            const link = document.createElement("a");
+            link.classList.add("header-menu-item");
+            menuItem.classList.add("menu-link");
+            link.innerText = section.label;
+            link.setAttribute("href", "#" + section.id);
+            menuItem.appendChild(link);
+            menuList.appendChild(menuItem);
+
+            link.addEventListener("click", function (event) {
+                event.preventDefault();
+                document.getElementById(section.id).scrollIntoView({ behavior: "smooth" });
+                menu.style.display = "none";
+            });
+        });
+
+        menu.appendChild(menuList);
+    }
+
+    generateMenuItems();
+});
